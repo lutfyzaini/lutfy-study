d89cc017d5d6b4fe5c3c3078710f52446b753a4a
lutfyzaini <lutfyzaini5@gmail.com>
2025-02-11 20:05:06 +0700

    latihan 3 if else

---
+++ javascript/if.js
@@ -0,0 +1,11 @@
+var x = 10;
+var y = 6;
+var z = 1;
+
+for (z; z <= x; z++) {
+  if (z <= y) {
+    console.log("Angkot No. " + z + " beroperasi dengan baik.");
+  } else {
+    console.log("Angkot No. " + z + " sedang tidak beroperasi.");
+  }
+}
