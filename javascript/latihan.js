916d0b99b4992f41f2a84f8902bc9fd1154f62a8
lutfyzaini <lutfyzaini5@gmail.com>
2025-02-11 20:48:42 +0700

    latihan 6

--- javascript/latihan.js
+++ javascript/latihan.js
@@ -1,9 +1,9 @@
 var x = 10;
 var y = 6;
 for (var z = 1; z <= x; z++) {
-  if (z <= y) {
+  if (z <= y && z !== 5) {
     console.log("Angkot No. " + z + " beroperasi dengan baik.");
-  } else if (z === 8) {
+  } else if (z === 8 || z === 10 || z === 5) {
     console.log("Angkot No. " + z + " sedang lembur.");
   } else {
     console.log("Angkot No. " + z + " sedang tidak beroperasi.");
