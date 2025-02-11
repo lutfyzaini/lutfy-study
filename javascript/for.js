70c088615f036420fd3836d147c284696560c3e4
lutfyzaini <lutfyzaini5@gmail.com>
2025-02-11 19:28:48 +0700

    latihan 2

---
+++ javascript/for.js
@@ -0,0 +1,12 @@
+var x = 20;
+var y = 1;
+var z = 10;
+
+while (y <= z) {
+  console.log("Angkot No. " + y + " beroperasi dengan baik.");
+  y++;
+}
+
+for (z = z + 1; z <= x; z++) {
+  console.log("Angkot No. " + z + " sedang tidak beroperasi.");
+}
