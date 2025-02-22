0492a7e53eaf9cf490858afcd62548842e6979c8
lutfyzaini <lutfyzaini5@gmail.com>
2025-02-22 13:17:06 +0700

    function sederhana

--- javascript/function.js
+++ javascript/function.js
@@ -1,8 +1,8 @@
-function total(a, b) {
-  var total;
-  total = a + b;
+function kubus(a) {
+  var jumlah;
+  jumlah = a * a * a;
 
-  return total;
+  return jumlah;
 }
 
-
+console.log("kubus a = " + kubus(8) + "\nkubus b = " + kubus(3) + "\njumlah antara kubus a + kubus b adalah = " + (kubus(8) + kubus(3)));
