6bef9602d06a23db76abd63d650d7220633f1996
lutfyzaini <lutfyzaini5@gmail.com>
2025-02-23 16:23:37 +0700

    parameter dan argumen1

--- javascript/function.js
+++ javascript/function.js
@@ -5,4 +5,5 @@ function kubus(a) {
   return jumlah;
 }
 
-console.log("kubus a = " + kubus(8) + "\nkubus b = " + kubus(3) + "\njumlah antara kubus a + kubus b adalah = " + (kubus(8) + kubus(3)));
+console.log("kubus a = " + kubus(8) + "\nkubus b = " + kubus(3) + "\njumlah antara kubus a + kubus b  adalah = " + (kubus(8) + kubus(3)));
+   
---
+++ javascript/function2.js
@@ -0,0 +1,9 @@
+function coba(a, b) {
+  return a + b;
+}
+
+var a = parseInt(prompt("Masukkan nilai 1 : "));
+var b = parseInt(prompt("Masukkan nilai 2 : "));
+
+var hasil = coba(a, b);
+console.log(coba(100, 70));
