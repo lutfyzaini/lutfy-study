ccf74bae7e158fdccd5268edfad9fe53952537a1
lutfyzaini <lutfyzaini5@gmail.com>
2025-02-24 15:59:00 +0700

    Modifikasi pengulangan saat di cancel

--- javascript/wordCheck.js
+++ javascript/wordCheck.js
@@ -1,7 +1,22 @@
-var input = prompt("Masukan sebuah kalimat : ");
+// var ulang = true;
+while (true) {
+  var input = prompt("Masukan sebuah kalimat : ") || "";
+  if (input === null || input.trim() === "") {
+    alert("Terimakasih!");
+    break;
+  } else if (input.trim() === "") {
+    alert("Input tidak boleh kosong");
+    continue;
+  } else {
+    function wordCount(input) {
+      return input.trim() === "" ? 0 : input.trim().split(/\s+/).length;
+    }
 
-function wordCount(input) {
-  return input.split(" ").length;
+    alert("Jumlah kata dalam kalimat '" + input + "' adalah " + wordCount(input));
+    // ulang = confirm("Coba lagi?");
+    if (!confirm("Coba lagi?")) {
+      alert("Terimakasih!");
+      break;
+    }
+  }
 }
-
-alert("Jumlah kata dalam kalimat " + input + " adalah " + wordCount(input));
