062f7ac95f31668f70c0b51103a984762f494821
lutfyzaini <lutfyzaini5@gmail.com>
2025-03-01 14:50:23 +0700

    membuat program kelola angkot

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,21 @@
-alert("Hello World!");
-prompt
+var penumpang = ["Lutfy", undefined, "Zaini"];
+var tambahPenumpang = function (namaPenumpang, penumpang) {
+  // Jika angkot kosong
+  if (penumpang.length == 0) {
+    penumpang.push(namaPenumpang);
+    return penumpang;
+  } else {
+    for (let i = 0; i < penumpang.length; i++) {
+      if (penumpang[i] == undefined) {
+        penumpang[i] = namaPenumpang;
+        return penumpang;
+      } else if (penumpang[i] == namaPenumpang) {
+        console.log(namaPenumpang + "SUDAH ADA DI DALAM ANGKOT");
+        return penumpang;
+      } else if (i == penumpang.length - 1) {
+        penumpang.push(namaPenumpang);
+        return penumpang;
+      }
+    }
+  }
+};
