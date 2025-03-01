b0408e5bcc14a4021803a63f1d686186d4407ebb
lutfyzaini <lutfyzaini5@gmail.com>
2025-03-02 06:15:09 +0700

    cara membuat object

--- javascript/script1.js
+++ javascript/script1.js
@@ -1,10 +1,37 @@
+// Mebuat object
+// Object literal
 var mhs = {
   nama: "Lutfy",
-  umur: 28,
-  ips: [3.05, 3.5, 3, 90],
-  alamat: {
-    jalan: "Lurung Tengah RT022/RW03, Dk.Rejosari, Cabeankunti",
-    kota: "Boyolali",
-    Provinsi: "Jawa Barat",
-  },
+  nrp: "12312313",
+  email: "wasdwasd@sinus.ac.id",
+  jurusan: "Informatika",
 };
+var mhs1 = {
+  nama: "Zaini",
+  nrp: "154545413",
+  email: "wlkjkljkasd@sinus.ac.id",
+  jurusan: "Informatika",
+};
+
+// Function Declaration
+function buatObjectMhs(nama, nrp, email, jurusan) {
+  var mhs = {};
+  mhs.nama = nama;
+  mhs.nrp = nrp;
+  mhs.email = email;
+  mhs.jurusan = jurusan;
+
+  return mhs;
+}
+
+var mhs3 = buatObjectMhs("Toni", "878786690", "tonisa@sinus.ac.id", "Sistem Informasi");
+
+// Constructor
+function Mahasiswa(nama, nrp, email, jurusan) {
+  this.nama = nama;
+  this.nrp = nrp;
+  this.email = email;
+  this.jurusan = jurusan;
+}
+
+var mhs4 = new Mahasiswa("Dapa", "73738493", "daoasdafa@sinus.ac.id", "Sistem Informasi");
