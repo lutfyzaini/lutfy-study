9de02fedfaf68cc4fa80a7c178ed810e41f86347
lutfyzaini <lutfyzaini5@gmail.com>
2025-03-02 05:56:10 +0700

    Intro object

---
+++ javascript/script1.js
@@ -0,0 +1,10 @@
+var mhs = {
+  nama: "Lutfy",
+  umur: 28,
+  ips: [3.05, 3.5, 3, 90],
+  alamat: {
+    jalan: "Lurung Tengah RT022/RW03, Dk.Rejosari, Cabeankunti",
+    kota: "Boyolali",
+    Provinsi: "Jawa Barat",
+  },
+};
