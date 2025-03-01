d7797a20684300d69dd2b247d858c67eec10fc38
lutfyzaini <lutfyzaini5@gmail.com>
2025-03-02 06:38:48 +0700

    this pada object

--- javascript/script1.js
+++ javascript/script1.js
@@ -1,37 +1,30 @@
-// Mebuat object
-// Object literal
-var mhs = {
-  nama: "Lutfy",
-  nrp: "12312313",
-  email: "wasdwasd@sinus.ac.id",
-  jurusan: "Informatika",
-};
-var mhs1 = {
-  nama: "Zaini",
-  nrp: "154545413",
-  email: "wlkjkljkasd@sinus.ac.id",
-  jurusan: "Informatika",
-};
+// Membuat object
+// this
 
-// Function Declaration
-function buatObjectMhs(nama, nrp, email, jurusan) {
-  var mhs = {};
-  mhs.nama = nama;
-  mhs.nrp = nrp;
-  mhs.email = email;
-  mhs.jurusan = jurusan;
+// // cara - 1 - function declaration
+// function halo() {
+//   console.log(this));
+//   console.log("Halo");
+// }
+// this.halo();
 
-  return mhs;
+// this mengembalikan object Global
+
+// // cara - 2 - object literal
+// var obj = { a: 10, nama: "Lutpi" };
+// obj.halo = function () {
+//   console.log(this);
+//   console.log("Halo");
+// };
+// obj.halo();
+
+// this mengembalikan object yang bersangkutan
+
+// cara - 3 - constructor
+function Halo() {
+  console.log(this);
+  console.log("Halo");
 }
-
-var mhs3 = buatObjectMhs("Toni", "878786690", "tonisa@sinus.ac.id", "Sistem Informasi");
-
-// Constructor
-function Mahasiswa(nama, nrp, email, jurusan) {
-  this.nama = nama;
-  this.nrp = nrp;
-  this.email = email;
-  this.jurusan = jurusan;
-}
-
-var mhs4 = new Mahasiswa("Dapa", "73738493", "daoasdafa@sinus.ac.id", "Sistem Informasi");
+var obj1 = new Halo();
+var obj2 = new Halo();
+// this mengembalikan object yang baru dibuat
