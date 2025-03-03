e5c4f2094bbdb66202bc4c0e3eb15d736e42a8ad
lutfyzaini <lutfyzaini5@gmail.com>
2025-03-03 19:05:04 +0700

    belajar object

---
+++ javascript/obj.js
@@ -0,0 +1,9 @@
+function Buku(judul, penulis, tahunTerbit, genre, rating) {
+  this.judul = judul;
+  this.penulis = penulis;
+  this.tahunTerbit = tahunTerbit;
+  this.genre = genre;
+  this.rating = rating;
+}
+
+var buku1 = new Buku("Esensialism", "Greg Mckeown", 2023, "Self Improvement", "-/10");
