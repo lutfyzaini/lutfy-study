dc5604d28aa0e6c157e4d112024fa0e2f050ade3
lutfyzaini <lutfyzaini5@gmail.com>
2025-03-04 14:27:39 +0700

    program buku

--- javascript/obj.js
+++ javascript/obj.js
@@ -4,6 +4,25 @@ function Buku(judul, penulis, tahunTerbit, genre, rating) {
   this.tahunTerbit = tahunTerbit;
   this.genre = genre;
   this.rating = rating;
+
+  this.tampilkanInfo = function () {
+    console.log(`Judul : ${this.judul}, Penulis : ${this.penulis}, Tahun : ${this.tahunTerbit}, Genre : ${this.genre}, Rating : ${this.rating}`);
+
+    for (let i = 0; i < this.tampilkanInfo.length; i++) {
+      console.log("Buku ke " + " : " + this.tampilkanInfo[i]);
+    }
+  };
+
+  this.ubahRating = function (ratingBaru) {
+    this.rating = ratingBaru;
+    console.log(`Rating buku "${this.judul}" telah diperbarui menjadi ${this.rating}`);
+  };
+
+  // this.tambahBuku = function (judul, penulis, tahunTerbit, genre, rating) {
+  //   this.tambahBuku = new Buku(judul, penulis, tahunTerbit, genre, rating);
+  // };
 }
 
-var buku1 = new Buku("Esensialism", "Greg Mckeown", 2023, "Self Improvement", "-/10");
+var buku1 = new Buku("Esensialism", "Greg Mckeown", 2023, "Self Improvement", "8.5/10");
+var buku2 = new Buku("Filosofi Teras", "Henry Manampiring", 2023, "Self Improvement", "9/10");
+var buku3 = new Buku("Meditations", "Marcus Aurelius", 2023, "Filsafat", "10/10");
