e7abc772d6b8933dd52e2111849b675a43ed4239
lutfyzaini <lutfyzaini5@gmail.com>
2025-03-16 20:58:58 +0700

    Intro Python - loop and data stucture

---
+++ cs50/python/dictionaries.py
@@ -0,0 +1,5 @@
+houses = {"Harry" : "Gryffindor", "Draco": "Slytherin" }
+
+houses["Herminoe"] = "Gryffindor"
+
+print(houses["Herminoe"])
--- cs50/python/loops.py
+++ cs50/python/loops.py
@@ -1,2 +1,3 @@
-for i in [0,1,2,3,4,5]:
-    print(i)
+name  = "Harry"
+for character in name:
+    print(character)
